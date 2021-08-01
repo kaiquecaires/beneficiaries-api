c648f6d2d054490c4a8c549ac1c20db07f7e62fa
kaique caires <kaique.caires@yahoo.com>
2021-08-01 10:38:43 -0300

    test: ensure Should return status code 500 if delete function in beneficiary repository throws

--- tests/controllers/BeneficiariesController/deleteBeneficiary.spec.js
+++ tests/controllers/BeneficiariesController/deleteBeneficiary.spec.js
@@ -23,6 +23,27 @@ const makeHttpRequest = () => ({
 })
 
 describe('Test delete function in BeneficiaryController', () => {
+  test('Should return status code 400 if id is not provided', async () => {
+    const { beneficiaryController, fakeBeneficiaryRepository } = makeSut()
+    const httpRequest = makeHttpRequest()
+    const httpResponse = await beneficiaryController.delete(null)
+    await fakeBeneficiaryRepository.findByCpf(httpRequest.body.cpf)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('Should return status code 500 if delete function in beneficiary repository throws', async () => {
+    const { beneficiaryController, fakeBeneficiaryRepository } = makeSut()
+    jest.spyOn(fakeBeneficiaryRepository, 'delete').mockImplementation(() => {
+      throw new Error()
+    })
+    const httpRequest = makeHttpRequest()
+    await beneficiaryController.create(httpRequest.body)
+    const beneficiary = await fakeBeneficiaryRepository.findByCpf(httpRequest.body.cpf)
+    const httpResponse = await beneficiaryController.delete(beneficiary._id)
+    await fakeBeneficiaryRepository.findByCpf(httpRequest.body.cpf)
+    expect(httpResponse.statusCode).toBe(500)
+  })
+
   test('Should return status code 204 if beneficiary was deleted', async () => {
     const { beneficiaryController, fakeBeneficiaryRepository } = makeSut()
     const httpRequest = makeHttpRequest()
@@ -31,13 +52,5 @@ describe('Test delete function in BeneficiaryController', () => {
     const httpResponse = await beneficiaryController.delete(beneficiary._id)
     await fakeBeneficiaryRepository.findByCpf(httpRequest.body.cpf)
     expect(httpResponse.statusCode).toBe(204)
-  })
-
-  test('Should return status code 400 if id is not provided', async () => {
-    const { beneficiaryController, fakeBeneficiaryRepository } = makeSut()
-    const httpRequest = makeHttpRequest()
-    const httpResponse = await beneficiaryController.delete(null)
-    await fakeBeneficiaryRepository.findByCpf(httpRequest.body.cpf)
-    expect(httpResponse.statusCode).toBe(400)
   })
 })
