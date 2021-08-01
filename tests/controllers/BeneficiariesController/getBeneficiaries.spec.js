db6ef9700b252163a5529a77fd1ae2157c970866
kaique caires <kaique.caires@yahoo.com>
2021-08-01 10:46:16 -0300

    test: ensure Should return all beneficiaries

---
+++ tests/controllers/BeneficiariesController/getBeneficiaries.spec.js
@@ -0,0 +1,37 @@
+const BeneficiaryController = require('../../../src/controllers/BeneficiariesController')
+const FakeBeneficiaryRepository = require('../../repositories/FakeBeneficiaryRepository')
+
+const makeSut = () => {
+  const fakeBeneficiaryRepository = new FakeBeneficiaryRepository()
+  const beneficiaryController = new BeneficiaryController(fakeBeneficiaryRepository)
+
+  return {
+    beneficiaryController,
+    fakeBeneficiaryRepository
+  }
+}
+
+const makeHttpRequest = () => ({
+  body: {
+    name: 'John Doe',
+    cpf: '00000000272',
+    rg: 'xxxxxxxxx',
+    date_of_birth: new Date().toISOString(),
+    type_of_plan: 'Basic',
+    number_of_dependents: 0
+  }
+})
+
+describe('Test findById and findAll function in BeneficiaryController', () => {
+  test('Should return all beneficiaries', async () => {
+    const { beneficiaryController } = makeSut()
+    const httpRequest = makeHttpRequest()
+    await beneficiaryController.create(httpRequest.body)
+    const httpRequest2 = makeHttpRequest()
+    httpRequest2.body.cpf = '70334208033'
+    await beneficiaryController.create(httpRequest2.body)
+    const httpResponse = await beneficiaryController.findAll()
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body.length).toBe(2)
+  })
+})
