fe8515e9413167f6e8f5b2b6a0b1cf224017b671
kaique caires <kaique.caires@yahoo.com>
2021-08-01 08:16:57 -0300

    test: ensure Should return status code 400 if cpf is not provided

--- tests/controllers/BeneficiariesController/createBeneficiary.test.js
+++ tests/controllers/BeneficiariesController/createBeneficiary.test.js
@@ -28,4 +28,11 @@ describe('Test create function in BeneficiaryController', () => {
     const httpResponse = await beneficiaryController.create(httpRequest.body)
     expect(httpResponse.statusCode).toBe(400)
   })
+
+  test('Should return status code 400 if cpf is not provided', async () => {
+    const { beneficiaryController } = makeSut()
+    httpRequest.body.cpf = null
+    const httpResponse = await beneficiaryController.create(httpRequest.body)
+    expect(httpResponse.statusCode).toBe(400)
+  })
 })
